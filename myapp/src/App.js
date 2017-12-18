2dcd2c69f0dbb55c10d12986674f91bff82e674e
xiaofei <xfhe19841104@gmail.com>
2017-12-18 15:31:32 +0800

    12.1

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Button } from 'antd';
+import PcHeader from "./js/components/pc_header";
 // import Button from 'antd/lib/button';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Button type="primary">Primary</Button>
-                <Button>Default</Button>
-                <Button type="dashed">Dashed</Button>
-                <Button type="danger">Danger</Button>
+            <div>
+                <PcHeader/>
             </div>
         );
     }
