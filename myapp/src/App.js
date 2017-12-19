799245b7f64afc2eee425e2d3f2de83725f5b5d9
xiaofei <xfhe19841104@gmail.com>
2017-12-19 11:11:15 +0800

    12.3

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Button } from 'antd';
 import PcHeader from "./js/components/pc_header";
+import PcFooter from './js/components/pc_footer';
 import MobileHeader from './js/components/mobile_header';
 // import Button from 'antd/lib/button';
 
@@ -13,6 +14,7 @@ class App extends Component {
             <div>
                 <MediaQuery query='(min-device-width: 1224px)'>
                     <PcHeader/>
+                    <PcFooter/>
                 </MediaQuery>
 
                 <MediaQuery query='(max-device-width: 1224px)'>
