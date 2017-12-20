ff59a9f5b2879fee3f20532918bc32d9b280308d
xiaofei <xfhe19841104@gmail.com>
2017-12-20 14:17:48 +0800

    13-2

--- myapp/src/js/components/mobile_header.js
+++ myapp/src/js/components/mobile_header.js
@@ -1,15 +1,37 @@
 import React from 'react'
 import news from '../../images/news.png'
 import './../../css/mobile_header.css';
+import { Link } from 'react-router-dom';
+import { Icon } from 'antd';
 
 class MobileHeader extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            current: 'top',
+            modalVisible: false,
+            action: 'login',
+            hasLogined: false,
+            userNickName: '',
+            userid: 0,
+        }
+    };
+
     render() {
+        const userShow = this.state.hasLogined ?
+            <Link>
+                <Icon type='inbox'/>
+            </Link>
+            :
+            <Icon type='setting'/>
+
         return(
             <div id='mobileheader'>
                 <header>
                     <img src={ news } alt="logo"/>
                     <span>ReactNews</span>
+                    {userShow}
                 </header>
             </div>
         );
