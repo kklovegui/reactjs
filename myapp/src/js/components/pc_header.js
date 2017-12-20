628ed7f8d57d760acccff42d3a1a343f7842a697
xiaofei <xfhe19841104@gmail.com>
2017-12-20 10:35:24 +0800

    13-1-2

--- myapp/src/js/components/pc_header.js
+++ myapp/src/js/components/pc_header.js
@@ -9,6 +9,8 @@ const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 const FormItem = Form.Item;
 
+const TabPane = Tabs.TabPane;
+
 class PcHeader extends React.Component {
 
     constructor() {
@@ -21,10 +23,30 @@ class PcHeader extends React.Component {
             userNickName: '',
             userid: 0,
         }
+    };
+
+    setModalVisible(value) {
+        this.setState({
+            modalVisible: value
+        });
+    };
+
+    handleClick(e) {
+        if (e.key == 'register'){
+            this.setState({current: 'register'});
+            this.setModalVisible(true);
+        }else {
+            this.setState({current: e.key});
+        }
+    };
+
+    handleSubmit(e){
+
     }
 
     render() {
         let { getFieldProps } = this.props.form;
+        console.log(this.props.form);
         const userShow = this.state.hasLogined ?
             <Menu.Item key="logout" className="register">
                 <Button type="primary" htmlType="button">
@@ -53,7 +75,7 @@ class PcHeader extends React.Component {
                         </a>
                     </Col>
                     <Col span={16} className="items">
-                        <Menu mode="horizontal" selectedKeys={[this.state.current]}>
+                        <Menu mode="horizontal" onClick={this.handleClick.bind(this)} selectedKeys={[this.state.current]}>
                             <Menu.Item key="top">
                                 <Icon type="appstore"/>头条
                             </Menu.Item>
@@ -87,8 +109,26 @@ class PcHeader extends React.Component {
                            wrapClassName="vertical-center-modal"
                            visible={this.state.modalVisible}
                            onCancel={()=>this.setModalVisible(false)}
-                           onOk={()=>this.setModalVisible(false) okText="关闭"}
-                            />
+                           onOk={()=>this.setModalVisible(false)}
+                           okText="关闭"
+                    >
+                        <Tabs type='card'>
+                            <TabPane tab='注册' key='2'>
+                                <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+                                    <FormItem label='账户'>
+                                        <Input placeholder='请输入账户' {...getFieldProps('r_userName')}/>
+                                    </FormItem>
+                                    <FormItem label='密码'>
+                                        <Input type='password' placeholder='请输入密码' {...getFieldProps('r_password')}/>
+                                    </FormItem>
+                                    <FormItem label='确认密码'>
+                                        <Input type='password' placeholder='在此输入密码' {...getFieldProps('r_confirmPassword')}/>
+                                    </FormItem>
+                                    <Button type='primary' htmlType='submit'>注册</Button>
+                                </Form>
+                            </TabPane>
+                        </Tabs>
+                    </Modal>
                     <Col span={2}/>
                 </Row>
             </header>
